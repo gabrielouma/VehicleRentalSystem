cac72b51c4098a4bdd313e3e99a2b7f7dd805d00
Gabriel Ouma <oumagaby01@gmail.com>
2024-09-19 03:02:20 +0300

    initial commit

--- Vehicle_Rental_System/settings.py
+++ Vehicle_Rental_System/settings.py
@@ -53,7 +53,6 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -146,14 +145,12 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [STATIC_DIR]
-MEDIA_URL = '/staticfiles/'
+MEDIA_URL = '/images/'
 
 STATICFILES_DIRS  = [os.path.join(BASE_DIR, 'static')]
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles',)
-
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field

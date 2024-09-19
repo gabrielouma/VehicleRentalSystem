ddab0026e4989811e1a31afbc2f8081c3ecb9c7f
Gabriel Ouma <oumagaby01@gmail.com>
2024-09-19 12:46:26 +0300

    initial commit

--- Vehicle_Rental_System/settings.py
+++ Vehicle_Rental_System/settings.py
@@ -93,6 +93,8 @@ USE_TZ = True
 STATIC_URL = '/static/'
 MEDIA_URL = '/images/'
 
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'images')
--- Vehicle_Rental_System/urls.py
+++ Vehicle_Rental_System/urls.py
@@ -24,5 +24,5 @@ urlpatterns = [
     path('', include('rentapp.urls')),
 ]
 
+urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

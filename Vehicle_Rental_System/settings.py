e181c3da5e58ff49b33a5a7d4945ff04048d37f8
Gabriel Ouma <oumagaby01@gmail.com>
2024-09-19 03:22:49 +0300

    initial commit

--- Vehicle_Rental_System/settings.py
+++ Vehicle_Rental_System/settings.py
@@ -20,9 +20,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 TEMPLATE_DIR = BASE_DIR/'templates'
 STATIC_DIR = BASE_DIR/'static'
 
-STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+
 
 # Save Image in this directory
 MEDIA_ROOT = os.path.join(STATIC_DIR, 'profile_picture')
@@ -143,14 +142,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATICFILES_DIRS = [STATIC_DIR]
-MEDIA_URL = '/images/'
+MEDIA_URL = '/static/images'
 
 STATICFILES_DIRS  = [os.path.join(BASE_DIR, 'static')]
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles',)
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles', 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field

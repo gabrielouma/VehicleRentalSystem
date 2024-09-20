61179e1fff4071a54a4edc39b3ba2bf7b721beba
Gabriel Ouma <oumagaby01@gmail.com>
2024-09-20 13:07:07 +0300

    initial commit

--- Vehicle_Rental_System/settings.py
+++ Vehicle_Rental_System/settings.py
@@ -75,7 +75,7 @@ WSGI_APPLICATION = 'Vehicle_Rental_System.wsgi.application'
 #     },
 # }
 DATABASES = {
-    "default": dj_database_url.parse(os.environ.get("DATABASE_URL"))
+    "default": dj_database_url.parse(os.environ.get("DATABASE_URL", "sqlite:///db.sqlite3"))
 }
 
 
